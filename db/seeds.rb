a8f3fc04f84542bce7b92a3c2cd7e8ad58f5ea3f
Brendan Hanks <ex.actionmodel@gmail.com>
2011-08-11 10:49:17 -0500

    Changed seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@
 #
 # Examples:
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+#   cities = City.create([{ :name => "Chicago" }, { :name => "Copenhagen" }])
+#   Mayor.create(:name => "Daley", :city => cities.first)
 
-Venue.create!(:name=>'Gabe's', :address=>'330 East Washington Street', :site_url=>'http://www.iowacitygabes.com/', :display_name=>'Gabe's')
-Venue.create!(:name=>'The Mill', :address=>'120 E. Burlington Street', :site_url=>'http://icmill.com/', :display_name=>'Mill')
-Venue.create!(:name=>'Blue Moose Taphouse', :address=>'211 Iowa Ave', :site_url=>'http://bluemooseic.com/', :display_name=>'Blue Moose')
-Venue.create!(:name=>'The Yacht Club', :address=>'13 S Linn St', :site_url=>'http://www.iowacityyachtclub.org/', :display_name=>'Yacht Club')
-Venue.create!(:name=>'The Englert', :address=>'221 East Washington Street', :site_url=>'http://www.englert.org/', :display_name=>'Englert')
+Venue.create!(:name=>"Gabe's", :address=>"330 East Washington Street", :site_url=>"http://www.iowacitygabes.com/", :display_name=>"Gabe's")
+Venue.create!(:name=>"The Mill", :address=>"120 E. Burlington Street", :site_url=>"http://icmill.com/", :display_name=>"Mill")
+Venue.create!(:name=>"Blue Moose Taphouse", :address=>"211 Iowa Ave", :site_url=>"http://bluemooseic.com/", :display_name=>"Blue Moose")
+Venue.create!(:name=>"The Yacht Club", :address=>"13 S Linn St", :site_url=>"http://www.iowacityyachtclub.org/", :display_name=>"Yacht Club")
+Venue.create!(:name=>"The Englert", :address=>"221 East Washington Street", :site_url=>"http://www.englert.org/", :display_name=>"Englert")
